d37b86ffba82cb427a0ae9117ae455b4e7158ba2
Oleksiy Penkov <44129586+OleksiyPenkov@users.noreply.github.com>
2020-03-05 15:32:04 +0800

    * Refactoring (FillLayers)

--- Math/unit_materials.pas
+++ Math/unit_materials.pas
@@ -156,7 +156,7 @@ var
   Stack, Layer: PVirtualNode;
   Data: PRowData;
 
-  i, j, NM, NL, CurrentLayer, g: integer;
+  NM, NL, CurrentLayer: integer;
 
   LayersCount: integer;
   Substrate, Material: TMaterial;
@@ -167,150 +167,181 @@ var
   Inside: boolean;
   InsideMain: Boolean;
 
+  procedure FillMaterial;
+  begin
+    Material := FindMaterial(Data.Text);
+
+    if Data.r <> '' then
+        Material.ro := StrToFloat(Data.r);
+    if Data.s <> '' then
+    begin
+      Material.s := SetFun(Data.s);
+      Material.s.a := Material.s.a / 1.41;
+      Material.s.b := Material.s.b / 1.41;
+    end
+    else
+    begin
+      Material.s.a := 0;
+      Material.s.b := 0;
+    end;
+  end;
+
+  procedure FillLayer;
+  var
+    g, i: integer;
+  begin
+    with Result[CurrentLayer] do
+    begin
+      L := StrToFloat(Data.H);
+
+      if (i = 1) and InsideMain then TotalD := TotalD + L;
+
+      for g := 0 to High(Gradients) do
+      begin
+        if Inside and (Data.Text = Gradients[g].ParentLayer) then
+        begin
+          case Gradients[g].Subj of
+            gsL : case Gradients[g].Form of
+                    gtLine : begin
+                               L := L * (1 + i/(NL)* Gradients[g].Rate);
+                               GradientValue := L;
+                             end;
+                  end;
+            gsS : case Gradients[g].Form of
+                    gtLine :begin
+                              Material.s.a  := Material.s.a * (1 + i/(NL)* Gradients[g].Rate);
+                              Material.s.b  := Material.s.a * (1 + i/(NL)* Gradients[g].Rate);
+                              GradientValue := Material.s.a;
+                            end;
+                  end;
+            gsRo: case Gradients[g].Form of
+                    gtLine : Material.ro := Material.ro * (1 + i/(NL)* Gradients[g].Rate)
+                  end;
+
+          end;
+          Chart.Series[g].AddXY(i, GradientValue);
+        end;
+      end;
+
+      c := kk * Material.ro / Material.am * sqr(Lambda);
+      Delta := Delta + c * Material.f.re / 2 * Material.h.a;
+      Material.f.re := 1 - Material.f.re * c;
+      Material.f.im := Material.f.im * c;
+
+      e := Material.f;
+      s := Material.s.a;
+    end;
+  end;
+
+  procedure FillSubstrate;
+  begin
+    with Substrate do // подложка
+    begin
+      Data := Tree.GetNodeData(Tree.GetLast);
+      if Data.RowType = rtSubstrate then
+      begin
+        ReadHenke(Data.Text, 0, Lambda, f, am, ro);
+        if Data.r <> '' then ro := StrToFloat(Data.r);
+        if Data.s <> '' then
+          s := SetFun(Data.s)
+        else
+          s.a := 0;
+      end
+      else
+      begin
+        ReadHenke('Si', 0, Lambda, f, am, ro);
+        s.a := 5;
+        s.b := 0;
+      end;
+      c := kk * (ro / am) * sqr(Lambda);
+      Result[LayersCount + 1].L := 1E8;
+      Result[LayersCount + 1].s := s.a;
+      Result[LayersCount + 1].e.re := 1 - f.re * c;
+      Result[LayersCount + 1].e.im := f.im * c;
+    end;
+  end;
+
+  function CountLayers: Integer;
+  begin
+    Result := 0;
+    Stack := Tree.GetFirst;
+    while Stack <> Nil do
+    begin
+      Data := Tree.GetNodeData(Stack);
+      if Data.RowType = rtStack then
+        inc(Result, Stack.ChildCount * Data.N);
+      Stack := Tree.GetNextSibling(Stack);
+    end;
+  end;
+
+
+  procedure FillStacks;
+  var
+    g, i, j: Integer;
+  begin
+    Stack := Tree.GetFirst;
+    while Stack <> Nil do
+    begin
+      Data := Tree.GetNodeData(Stack);
+      if Data.RowType = rtStack then
+      begin
+        NL := Data.N;
+
+        Inside := False;
+        for g := 0 to High(Gradients) do
+          if Data.Text = Gradients[g].ParentPeriod then
+          begin
+            Inside := True;
+            Break;
+          end;
+
+        InsideMain := Pos('Main', Data.Text) <> 0;
+        if InsideMain then TotalD := 0;
+
+        for i := 1 to NL do
+        begin
+          Layer := Stack.FirstChild;
+          for j := 0 to Stack.ChildCount - 1 do
+          begin
+            Data := Tree.GetNodeData(Layer);
+
+            FillMaterial;
+            FillLayer;
+
+            Layer := Tree.GetNextSibling(Layer);
+            inc(CurrentLayer);
+          end;
+        end;
+      end;
+      Stack := Tree.GetNextSibling(Stack);
+    end;
+  end;
+
+  procedure PrepareMaterials;
+  begin
+    Stack := Tree.GetFirst;
+    while Stack <> Nil do
+    begin
+      Data := Tree.GetNodeData(Stack);
+      if Data.RowType = rtLayer then
+        AddMaterial(Data, Lambda);
+      Stack := Tree.GetNext(Stack);
+    end;
+  end;
+
 begin
+  LayersCount := CountLayers;
+
   SetLength(Result, 0);
   SetLength(Materials, 0);
 
   PrepareChart(Chart);
-
-  LayersCount := 0;
-  Stack := Tree.GetFirst;
-  while Stack <> Nil do
-  begin
-    Data := Tree.GetNodeData(Stack);
-    if Data.RowType = rtStack then
-      inc(LayersCount, Stack.ChildCount * Data.N);
-    Stack := Tree.GetNextSibling(Stack);
-  end;
-
   SetLength(Result, LayersCount + 2);
   CurrentLayer := 1;
 
-  Stack := Tree.GetFirst;
-  while Stack <> Nil do
-  begin
-    Data := Tree.GetNodeData(Stack);
-    if Data.RowType = rtLayer then
-      AddMaterial(Data, Lambda);
-    Stack := Tree.GetNext(Stack);
-  end;
-
-  Stack := Tree.GetFirst;
-  while Stack <> Nil do
-  begin
-    Data := Tree.GetNodeData(Stack);
-    if Data.RowType = rtStack then
-    begin
-      NL := Data.N;
-
-      Inside := False;
-      for g := 0 to High(Gradients) do
-        if Data.Text = Gradients[g].ParentPeriod then
-        begin
-          Inside := True;
-          Break;
-        end;
-
-      InsideMain := Pos('Main', Data.Text) <> 0;
-      if InsideMain then TotalD := 0;
-
-      for i := 1 to NL do
-      begin
-        Layer := Stack.FirstChild;
-        for j := 0 to Stack.ChildCount - 1 do
-        begin
-          Data := Tree.GetNodeData(Layer);
-
-          Material := FindMaterial(Data.Text);
-
-          if Data.r <> '' then
-              Material.ro := StrToFloat(Data.r);
-          if Data.s <> '' then
-          begin
-            Material.s := SetFun(Data.s);
-            Material.s.a := Material.s.a / 1.41;
-            Material.s.b := Material.s.b / 1.41;
-          end
-          else
-          begin
-            Material.s.a := 0;
-            Material.s.b := 0;
-          end;
-
-          with Result[CurrentLayer] do
-          begin
-            L := StrToFloat(Data.H);
-
-            if (i = 1) and InsideMain then TotalD := TotalD + L;
-
-            for g := 0 to High(Gradients) do
-            begin
-              if Inside and (Data.Text = Gradients[g].ParentLayer) then
-              begin
-                case Gradients[g].Subj of
-                  gsL : case Gradients[g].Form of
-                          gtLine : begin
-                                     L := L * (1 + i/(NL)* Gradients[g].Rate);
-                                     GradientValue := L;
-                                   end;
-                        end;
-                  gsS : case Gradients[g].Form of
-                          gtLine :begin
-                                    Material.s.a  := Material.s.a * (1 + i/(NL)* Gradients[g].Rate);
-                                    Material.s.b  := Material.s.a * (1 + i/(NL)* Gradients[g].Rate);
-                                    GradientValue := Material.s.a;
-                                  end;
-                        end;
-                  gsRo: case Gradients[g].Form of
-                          gtLine : Material.ro := Material.ro * (1 + i/(NL)* Gradients[g].Rate)
-                        end;
-
-                end;
-                Chart.Series[g].AddXY(i, GradientValue);
-              end;
-            end;
-
-            c := kk * Material.ro / Material.am * sqr(Lambda);
-            Delta := Delta + c * Material.f.re / 2 * Material.h.a;
-            Material.f.re := 1 - Material.f.re * c;
-            Material.f.im := Material.f.im * c;
-
-            e := Material.f;
-            s := Material.s.a;
-          end;
-
-          Layer := Tree.GetNextSibling(Layer);
-          inc(CurrentLayer);
-        end;
-      end;
-    end;
-    Stack := Tree.GetNextSibling(Stack);
-  end;
-
-  with Substrate do // подложка
-  begin
-    Data := Tree.GetNodeData(Tree.GetLast);
-    if Data.RowType = rtSubstrate then
-    begin
-      ReadHenke(Data.Text, 0, Lambda, f, am, ro);
-      if Data.r <> '' then ro := StrToFloat(Data.r);
-      if Data.s <> '' then
-        s := SetFun(Data.s)
-      else
-        s.a := 0;
-    end
-    else
-    begin
-      ReadHenke('Si', 0, Lambda, f, am, ro);
-      s.a := 5;
-      s.b := 0;
-    end;
-    c := kk * (ro / am) * sqr(Lambda);
-    Result[LayersCount + 1].L := 1E8;
-    Result[LayersCount + 1].s := s.a;
-    Result[LayersCount + 1].e.re := 1 - f.re * c;
-    Result[LayersCount + 1].e.im := f.im * c;
-  end;
+  PrepareMaterials;
+  FillStacks;
+  FillSubstrate;
 
   Result[0].L := 1E10;
   Result[0].e.re := 1;
